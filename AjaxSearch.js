739e5da209846b93f9c0bb38f228308dc4b691a2
themandysingh <mandy98@outlook.com>
2018-07-06 23:54:19 +0530

    js file connected but incomplete

---
+++ AjaxSearch.js
@@ -0,0 +1,20 @@
+(function() {
+    
+    const cities = [];
+    const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+    
+    window.onload = function() {
+        fetch(endpoint)
+            .then(data => data.json())
+            .then(finalData => cities.push(...finalData));
+        console.log(findMatches("Wes"));
+    }
+
+    function findMatches (wordToMatch, cities) {
+        return cities.filter(place => {
+            const regex = new RegExp(wordToMatch, 'gi');
+            return place.city.match(regex) || place.state.match(regex);
+        });
+    }
+
+})();
