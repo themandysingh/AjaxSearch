3db965bf1f2ea2d98ebcd562fd92201d9ff2089c
themandysingh <mandy98@outlook.com>
2018-07-07 13:49:48 +0530

    JS done. Minor CSS added

--- AjaxSearch.js
+++ AjaxSearch.js
@@ -2,12 +2,14 @@
     
     const cities = [];
     const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+    fetch(endpoint)
+        .then(data => data.json())
+        .then(finalData => cities.push(...finalData));
     
     window.onload = function() {
-        fetch(endpoint)
-            .then(data => data.json())
-            .then(finalData => cities.push(...finalData));
-        console.log(findMatches("Wes"));
+        const searchInput = document.querySelector(".search");
+        searchInput.addEventListener("change", displayMatches);
+        searchInput.addEventListener("keyup", displayMatches);
     }
 
     function findMatches (wordToMatch, cities) {
@@ -15,6 +17,25 @@
             const regex = new RegExp(wordToMatch, 'gi');
             return place.city.match(regex) || place.state.match(regex);
         });
+    }
+
+    function displayMatches() {
+        const suggestions = document.querySelector(".suggestions");
+        const matches = findMatches(this.value, cities);
+        const regex = new RegExp(this.value, "gi");
+        const html = matches.map(place => {
+            const cityName = place.city.replace(regex, `<span class = "hl">${this.value}</span>`);
+            const stateName = place.state.replace(regex, `<span class = "hl">${this.value}</span>`);
+            return `<li>
+                        <span class = "name"> ${cityName}, ${stateName} </span>
+                        <span class = "population"> ${numberWithCommas(place.population)} </span>
+                    </li>`;
+        }).join('');
+        suggestions.innerHTML = html;
+    }
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     }
 
 })();
